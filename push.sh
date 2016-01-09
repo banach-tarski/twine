6c6669743cd5109358ec2387d40f64b183c0eb0b
banach-tarski <voec@gmx.net>
2016-01-09 04:29:00 +0100

    ░░▒▒▓▓░░▒▒▓▓░░▒▒▓▓░░▒▒▓▓

--- push.sh
+++ push.sh
@@ -3,5 +3,5 @@ git remote add origin-ssh git@banach:banach-tarski/twine.git
 git pull
 git add .
 randomstring=$(cat /dev/urandom | tr -dc '░-▓' | fold -w 32 | head -n 1)
-git commit -m "░░▒▒▓▓`date +'%Y-%m-%d %H:%M:%S'`"+$randomstring+░░▒▒▓▓;
+git commit -m "░░▒▒▓▓░░▒▒▓▓░░▒▒▓▓░░▒▒▓▓";
 git push -u origin-ssh master
